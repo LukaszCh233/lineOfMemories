3b3c181a76fd66fa2111d10a89f4c7e81ec90e00
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2025-11-17 10:45:53 +0100

    add project to GitHub

---
+++ src/main/java/com/lineOfMemories/lineOfMemories/LineOfMemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.lineOfMemories.lineOfMemories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineOfMemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineOfMemoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lineOfMemories/lineOfMemories/LineOfMemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineOfMemories.lineOfMemories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineOfMemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
